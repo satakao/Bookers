1ffc62237a11930e7063d329fbd71531a0703e8a
TakaoSatou <142711130+satakao@users.noreply.github.com>
2023-10-12 14:43:01 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,29 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+  end
+  def create
+    book = Book.new(book_params)
+    if book.save
+       redirect_to book_path(book.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+    end
+  end
+  def update
+    book = book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(list.id)
   end
   def destroy
-    book
-    
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to 'books'
+  end
+  # private以下の呼び出しに制限をかける（urlに紐づかないように）
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_11_103222) do
+ActiveRecord::Schema.define(version: 2023_10_12_072823) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
